1ea14ad7b06316c5a82301f3fdfb8087a8645445
abdallahesam <abdallahesam@hotmail.com>
2020-05-08 22:05:20 +0300

    About to nuke this code to chang DesignCImage to AvaloniaDesignCImage

--- MainUI/MainWindow.xaml.cs
+++ MainUI/MainWindow.xaml.cs
@@ -64,6 +64,7 @@ namespace MainUI
             var rand = new Random();
             var images = p.Images.Where(i => i.Commodities.Count == 1).ToArray();
             images[rand.Next(images.Length)].TryDesign<SixLabors.ImageSharp.PixelFormats.Rgba32>(out var img);
+            img.BreakBetweenFrames = 1;
             var din = new DesigningWindow(img!);
             await din.ShowDialog(this);
             btnSettings.Content = "NOT IMPLEMENTED YET!";
